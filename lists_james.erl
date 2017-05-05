8c459d757b743856e0a6d183a6aab0a43ccee43e
James-Work <James.Timms@ee.co.uk>
2017-05-05 13:16:02 +0100

    Adding more lists_james.erl related functions defined while following futurelearn erlang course.

--- lists_james.erl
+++ lists_james.erl
@@ -1,14 +1,66 @@
 -module(lists_james).
 -export([sum/1, sum_tail/1, mul/1, mul_tail/1, max_list/1, double_tail/1,
-         double/1, evens/1, evens_tail/1,
+         double/1, evens/1, evens_tail/1, nub/1, bun/1, palindrom/1, palin/1,
+         nopunct/1,
          sum_tail/2, mul_tail/2, max_list/2, double_tail/2, evens_tail/2,
-         take/2]).
+         take/2, removeAll/2]).
 
+% Check if a string is a palindrome ignoring capitals and punctions.
+palindrom(Xs) -> palin(nocaps(nopunct(Xs))).
+
+% Remove capital letters from a string.
+nocaps([]) -> [];
+nocaps([X|Xs]) -> [nocap(X) | nocaps(Xs)].
+
+% Transform to none capital letter.
+nocap(X) ->
+  case $A =< X andalso X =< $Z of
+    true -> X+32;
+    false -> X
+  end.
+
+% Remove punction from a string.
+nopunct([]) -> [];
+nopunct([X|Xs]) ->
+  case lists:member(X, punctuation()) of
+    true -> nopunct(Xs);
+    false -> [X | nopunct(Xs)]
+  end.
+
+punctuation() -> ".,\ ;:\t\n'".
+
+% Check if a list is a literal palindrome.
+palin(Xs) -> Xs == reverse(Xs).
+
+% Reverse the order of a list.
+reverse(Xs) -> shunt(Xs, []).
+
+% Put elements in one list into another in reverse order.
+shunt([], Ys) -> Ys;
+shunt([X|Xs], Ys) -> shunt(Xs, [X|Ys]).
+
+% Remove all duplicate elements. Keeps the first occurances of an element.
+nub([]) -> [];
+nub([X|Xs]) -> [X|nub(removeAll(X, Xs))].
+
+% Remove all duplicates of an element.
+removeAll(_X, []) -> [];
+removeAll(X, [X|Xs]) -> removeAll(X, Xs);
+removeAll(X, [Y|Xs]) -> [Y|removeAll(X, Xs)].
+
+% Remove all duplicate elements. Keeps the last occurance of an element.
+bun([]) -> [];
+bun([X|Xs]) ->
+  case lists:member(X, Xs) of
+    true -> nub(Xs);
+    false -> [X | nub(Xs)]
+  end.
+
+% Take N elements of a list starting from the first element.
 take(_N, []) -> [];
-take(N, [X|Xs]) when N>0 -> [X | take(N-1, Xs)];
-take(N, [X|_Xs]) when N==0 -> [X];
+take(N, [X|Xs]) when N>0 -> [X|take(N-1, Xs)];
+take(N, [_X|_Xs]) when N==0 -> [];
 take(N, [_X|_Xs]) when N<0 -> {failed}.
-
 
 % doubles occurances of items in array.
 % [1,2,3] -> [1,1,2,2,3,3].
